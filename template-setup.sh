ba49fd42cf06e7868b0d9143f03726380ff76312
Orpheus Lummis <o@orpheuslummis.info>
2023-10-06 15:48:44 +0300

    add abstract-sdk app-template

---
+++ template-setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mv example.env .env
+
+# Delete this script after running
+rm -- "$0"
---
+++ tests/integration.rs
@@ -0,0 +1,61 @@
+use abstract_core::objects::{gov_type::GovernanceDetails, AccountId};
+use abstract_interface::{Abstract, AbstractAccount, AppDeployer, VCExecFns};
+use app::{
+    contract::{APP_ID, APP_VERSION},
+    msg::{AppInstantiateMsg, ConfigResponse},
+    *,
+};
+// Use prelude to get all the necessary imports
+use cw_orch::{anyhow, deploy::Deploy, prelude::*};
+
+use cosmwasm_std::Addr;
+
+// consts for testing
+const ADMIN: &str = "admin";
+
+// HACKMOS this is ow-orchestrator
+// HACKMOS daemon connect (pass the okp4 chain to the daemon)
+
+/// Set up the test environment with the contract installed
+fn setup() -> anyhow::Result<(AbstractAccount<Mock>, Abstract<Mock>, AppInterface<Mock>)> {
+    // Create a sender
+    let sender = Addr::unchecked(ADMIN);
+    // Create the mock
+    let mock = Mock::new(&sender);
+
+
+    // Construct the counter interface
+    let app = AppInterface::new(APP_ID, mock.clone());
+
+    // Deploy Abstract to the mock
+    let abstr_deployment = Abstract::deploy_on(mock, sender.to_string())?;
+
+    // Create a new account to install the app onto
+    let account =
+        abstr_deployment
+            .account_factory
+            .create_default_account(GovernanceDetails::Monarchy {
+                monarch: ADMIN.to_string(),
+            })?;
+
+    // claim the namespace so app can be deployed
+    abstr_deployment
+        .version_control
+        .claim_namespace(AccountId::local(1), "my-namespace".to_string())?;
+
+    app.deploy(APP_VERSION.parse()?)?;
+
+    account.install_app(app.clone(), &AppInstantiateMsg {}, None)?;
+
+    Ok((account, abstr_deployment, app))
+}
+
+#[test]
+fn successful_install() -> anyhow::Result<()> {
+    // Set up the environment and contract
+    let (_account, _abstr, app) = setup()?;
+
+    let config = app.config()?;
+    assert_eq!(config, ConfigResponse {});
+    Ok(())
+}
