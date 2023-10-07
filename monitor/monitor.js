ebaeec3023e2f072c3bb202217f8564158b411dd
Orpheus Lummis <o@orpheuslummis.info>
2023-10-07 12:16:17 +0300

    work by Huiqi

---
+++ monitor/monitor.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+const url = 'http://localhost:26657/status';
+
+async function fetchData() {
+  console.log("=======================================================");
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    const height = data.result["sync_info"]["latest_block_height"];
+    const url2 = "http://localhost:26657/block_results?height="+height;
+    const response2 = await fetch(url2);
+    const data2 = await response2.json();
+    const result = data2.result;
+    console.log(result);
+  } catch (error) {
+    console.error('There was a problem:', error.message);
+  }
+}
+
+setInterval(fetchData, 10000);
